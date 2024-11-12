8c6fe78c994d657b75f2619e396014b632f054c0
Srinidhi Krishnan <kadalazhisrinidhi2103@gmail.com>
2024-11-12 19:17:12 +0530

    Added Endpoints

--- index.js
+++ index.js
@@ -1,12 +1,80 @@
 // Including package and initializations
+
 const express = require("express");
 app = express();
+
+// DataBase
 
 // Middleware
 app.use(express.json());
 
-// Defining endpoints
+// Retrieving all user information  
+app.get("/products",(req,res)=>{                
+    res.json({
+        message: "Data retrieved"
+    })
+})
+
+ // Extracting information based on id
+app.get("/products/:id",(req,res)=>{            
+    res.json({
+        message: req.params.id
+    })
+})
+
+// Add new product details
+app.post("/products",(req,res)=>{               
+    res.json({
+        Name: "ABC",
+        "Description": "Hello",
+        "Price" : 10,
+        "Quantity": 10,
+    })
+})
+
+// Updates the information of a product based on ID
+app.put("/products/:id",(req,res)=>{            
+    res.json({
+        message: "Put method implemented"
+    })
+})
+
+// Deletes information of a given product based on id
+app.delete("/products/:id",(req,res)=>{
+    res.json({
+        message : "Delete method implemented"
+    })
+})
+
 
 
 // Starting server at given port
-app.listen(3000);
+app.listen(3000,console.log("Server Started...."));
+
+
+
+/*products = [
+    {
+        "Name" : "Srinidhi",
+        "Age"  : 20,
+        "Desc" : "This is a good",
+    },
+    {
+        "Name" : "Jhu",
+        "Age"  : 21,
+        "Desc" : "He is a boy",
+    },
+    {
+        "Name" : "Rogu",
+        "Age"  : 20,
+        "Desc" : "This is a good"
+    },
+    {
+        "Name" : "Abrar",
+        "Age"  : 19,
+        "Desc" : "He is a girl"
+    }
+];
+
+product = products.find(p=>p.Age == 21);
+console.log(product);*/
