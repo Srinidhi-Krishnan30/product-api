29756f6e7cfabac3d0be975294b6199a22c9dde3
Srinidhi Krishnan <kadalazhisrinidhi2103@gmail.com>
2024-11-12 05:51:09 +0530

    Basic files

---
+++ index.js
@@ -0,0 +1,12 @@
+// Including package and initializations
+const express = require("express");
+app = express();
+
+// Middleware
+app.use(express.json());
+
+// Defining endpoints
+
+
+// Starting server at given port
+app.listen(3000);
